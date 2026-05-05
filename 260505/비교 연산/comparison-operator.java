8767edf333eb64d00b4c037c748fcde6ced3b808
cece-297 <sooah0918@naver.com>
2026-05-05 18:42:43 +0900

    [Passed] 비교 연산 (130ms, 10MB)

--- 260505/비교 연산/comparison-operator.java
+++ 260505/비교 연산/comparison-operator.java
@@ -4,21 +4,19 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         int b = sc.nextInt();
-        int answer;
-        boolean one = a>=b;
-        boolean two = a>b;
-        boolean three = a<=b;
-        boolean four = a<b;
-        boolean five = a==b;
-        boolean six = a!=b;
-       
-       
-    System.out.println(one ? 1 : 0);
-    System.out.println(two ? 1 : 0);
-    System.out.println(three ? 1 : 0);
-    System.out.println(four ? 1 : 0);
-    System.out.println(five ? 1 : 0);
-    System.out.println(six ? 1 : 0);
+      
+        if(a>=b) {System.out.println(1);} 
+        else {System.out.println(0);}
+        if(a>b) {System.out.println(1);} 
+        else {System.out.println(0);}
+        if(a<=b) {System.out.println(1);} 
+        else {System.out.println(0);}
+        if(a<b) {System.out.println(1);} 
+        else {System.out.println(0);}
+        if(a==b) {System.out.println(1);} 
+        else {System.out.println(0);}
+        if(a!=b) {System.out.println(1);} 
+        else {System.out.println(0);}
     }
     
 }
