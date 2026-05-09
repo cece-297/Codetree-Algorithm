49294107902f99a9e597f840f5748e893da55427
cece-297 <sooah0918@naver.com>
2026-05-09 21:03:08 +0900

    [Passed] 윤년인가 (123ms, 10MB)

---
+++ 260509/윤년인가/is-leap-year.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int y = sc.nextInt();
+
+        if (y%400==0) {
+            System.out.println("true");
+        } else if ((y%100==0) && (y%400!=0)) {
+            System.out.println("false");
+        } else if (y%4==0) {
+            System.out.println("true");
+        } else {
+            System.out.println("false");
+        
+        }
+    }
+}
