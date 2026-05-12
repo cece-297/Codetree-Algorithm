4ee090642e969d0973bc0bfce3b9ad8dbddf6529
cece-297 <sooah0918@naver.com>
2026-05-12 16:40:21 +0900

    [Passed] 배열의 합 계산하기 (87ms, 9MB)

---
+++ 260512/배열의 합 계산하기/calculate-sum-of-array.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = {3,1,4,5,9};
+        int a = 1;
+        int b = 5;
+        int c = 9;
+        System.out.println(a+b+c);
+        // Please write your code here.
+    }
+}
