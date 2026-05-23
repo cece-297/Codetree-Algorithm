705b798146cce05268e6c384b16a480b255c4915
cece-297 <sooah0918@naver.com>
2026-05-23 17:22:58 +0900

    [Passed] 0 나올때까지 출력 (142ms, 10MB)

--- 260523/0 나올때까지 출력/print-until-zero-is-given.java
+++ 260523/0 나올때까지 출력/print-until-zero-is-given.java
@@ -2,18 +2,12 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-
-        while (a != 0) {
-            System.out.println(a);
-            a = sc.nextInt();
-            if (a == 0) {
-                break;
+        while (true) {    
+            int a = sc.nextInt();
+            if (a == 0) {break;}
+               System.out.println(a); 
             }
-                
-            }
-        
-       
+            
         // Please write your code here.
     }
 }
